30b23c85cbbd357012f8d43dcbb08acde1076393
Zichao Lin <earthjasonlin@163.com>
2023-01-18 09:55:25 +0800

    update appversion

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ const nodemailer = require('nodemailer')
 exports.NotificationURL = 'https://api-cloudgame.mihoyo.com/hk4e_cg_cn/gamer/api/listNotifications?status=NotificationStatusUnread&type=NotificationTypePopup&is_sort=true'
 exports.WalletURL = 'https://api-cloudgame.mihoyo.com/hk4e_cg_cn/wallet/wallet/get'
 exports.AnnouncementURL = 'https://api-cloudgame.mihoyo.com/hk4e_cg_cn/gamer/api/getAnnouncementInfo'
-exports.AppVersionURL = 'https://api-cloudgame-static.mihoyo.com/hk4e_cg_cn/gamer/api/getFunctionShieldNew?client_type=1'
+exports.AppVersionURL = 'https://sdk-static.mihoyo.com/hk4e_cn/mdk/launcher/api/resource?key=eYd89JmJ&launcher_id=18'
 
 exports.Notification = function(header) {
     let tmp = JSON.parse(request("GET",exports.NotificationURL,{
