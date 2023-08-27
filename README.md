f6f0a50c0ed2632870e139b5195b747109aaede2
HuanLinOTO <linsmc@126.com>
2023-08-27 20:02:57 +0800

    init

--- README.md
+++ README.md
@@ -1,10 +1,6 @@
 # GenshinCloudPlayHelpper
 
-每天帮你获取15分钟云原神时间~
-
-## ⚠️请不要进行宣传，谢谢！
-
-## 一旦发现宣传就删库跑路！
+每天帮你获取 15 分钟云原神时间~
 
 ## 配置教程
 
@@ -23,7 +19,7 @@
 }
 ```
 
-> sendMail   一个bool 代表是否在运行结束后将结果发送至指定邮箱，false时mailConfig内的内容可不填
+> sendMail   一个 bool 代表是否在运行结束后将结果发送至指定邮箱，false 时 mailConfig 内的内容可不填
 > mailConfig 具体配置
 > 
 > > user 发送方邮箱
@@ -36,7 +32,7 @@
 > > 
 > > smtpPort smtp 服务器端口
 > > 
-> > smtpSecure 如果smtp服务器使用SSL/TLS，那么为true，如果使用STARTTLS或不使用加密，那么为false
+> > smtpSecure 如果 smtp 服务器使用 SSL/TLS，那么为 true，如果使用 STARTTLS 或不使用加密，那么为 false
 
 对于常用的邮件而言，配置列举如下
 
@@ -67,30 +63,30 @@
 具体数据 请看“抓包”环节
 
 > token 是在云原神登录后用于验证的标记
-> client_type 1代表ios设备 2代表android&鸿蒙设备
+> client_type 1 代表 ios 设备 2 代表 android&鸿蒙设备
 > device_name 设备名称
 > device_model 设备型号
-> device_id 在米哈游服务器中注册的uuid
-> sys_version 系统版本 android&鸿蒙中为系统版本 ios设备中为ios版本
-> channel 下载渠道 ios设备填"app store" android&鸿蒙填"mihoyo"(以抓包为准，我也不知道渠道服是不是这个)
+> device_id 在米哈游服务器中注册的 uuid
+> sys_version 系统版本 android&鸿蒙中为系统版本 ios 设备中为 ios 版本
+> channel 下载渠道 ios 设备填"app store" android&鸿蒙填"mihoyo"（以抓包为准，我也不知道渠道服是不是这个）
 
 ### 抓包
 
 > > # ios
 > > 
-> > App Store中下载应用Stream（用于抓包）
+> > App Store 中下载应用 Stream（用于抓包）
 > > 然后看视频吧
 > > 看不了/不显示的点右边 https://hzsj.coding.net/p/ayakaturtleshop/d/PublicResource/git/raw/master/c9f1a9b91951b0f1c2da8f3817274074.mp4?download=false
 > > <video src="https://hzsj.coding.net/p/ayakaturtleshop/d/PublicResource/git/raw/master/c9f1a9b91951b0f1c2da8f3817274074.mp4?download=false"></video>
 > 
 > # android&鸿蒙
 > 
-> > (来自https://bili33.top/posts/MHYY-AutoCheckin-Manual)
-> > 因为云原神是在手机上运行的，所以你需要安装一个手机上的抓包软件（例如HttpCanary，或者如果你能够用fiddler电脑运行去抓也行）
+> > （来自 https://bili33.top/posts/MHYY-AutoCheckin-Manual)
+> > 因为云原神是在手机上运行的，所以你需要安装一个手机上的抓包软件（例如 HttpCanary，或者如果你能够用 fiddler 电脑运行去抓也行）
 > > [![看就完了](https://cdn.bilicdn.tk/gh/Vikutorika/assets@master/img/Github/MHYY-AutoCheckin/HTTPCANARY-Result.jpg?download=false)]
-> > 一定要记得装抓包软件提供的证书，要不然解不了SSL连接，一定要先登录并成功进去了再启动抓包软件！！！
+> > 一定要记得装抓包软件提供的证书，要不然解不了 SSL 连接，一定要先登录并成功进去了再启动抓包软件！！！
 > > [![看就完了](https://cdn.bilicdn.tk/gh/Vikutorika/assets@master/img/Github/MHYY-AutoCheckin/HTTPS-REQUEST-RESULT.png?download=false)]
-> > 这里面只要是个HTTP链接，随便一个里面都有我们所需要的东西，这里我就点开了一个链接，在请求里面有所有我们需要的东西，而解释我都写在图片里面了
+> > 这里面只要是个 HTTP 链接，随便一个里面都有我们所需要的东西，这里我就点开了一个链接，在请求里面有所有我们需要的东西，而解释我都写在图片里面了
 
 二者抓到的包差不多，抓到包之后有以下对应关系
 
@@ -107,8 +103,8 @@
 ```
 
 对着填进去就完了
-接着在configs文件夹里创建任意一个json文件，将配置填进去
-如果想一次性签到n个用户，创建不同的json文件即可，程序会自动扫描configs文件夹下面的文件
+接着在 configs 文件夹里创建任意一个 json 文件，将配置填进去
+如果想一次性签到 n 个用户，创建不同的 json 文件即可，程序会自动扫描 configs 文件夹下面的文件
 
 ## 另外
 
---
+++ src/config.js
@@ -0,0 +1,135 @@
+const { exit } = require("process")
+const fs = require("fs") 
+const nodemailer = require('nodemailer')
+const { default: axios } = require("axios")
+
+exports.NotificationURL = 'https://api-cloudgame.mihoyo.com/hk4e_cg_cn/gamer/api/listNotifications?status=NotificationStatusUnread&type=NotificationTypePopup&is_sort=true'
+exports.WalletURL = 'https://api-cloudgame.mihoyo.com/hk4e_cg_cn/wallet/wallet/get'
+exports.AnnouncementURL = 'https://api-cloudgame.mihoyo.com/hk4e_cg_cn/gamer/api/getAnnouncementInfo'
+// Here must be an earlier version so that the response won't be null
+exports.AppVersionURL = 'https://api-takumi.mihoyo.com/ptolemaios/api/getLatestRelease?app_id=1953443910&app_version=3.3.0&channel=mihoyo'
+
+exports.Notification = async function(header) {
+    let tmp = (await axios("GET",exports.NotificationURL,{
+        headers:header
+    })).data;
+    tmp.StringVersion = JSON.stringify(tmp);
+    return tmp;
+}
+exports.Wallet = async function(header) {
+    let tmp = (await axios("GET",exports.WalletURL,{
+        headers:header
+    })).data;
+    tmp.StringVersion = JSON.stringify(tmp);
+    return tmp;
+}
+exports.Announcement = async function(header) {
+    let tmp = (await axios("GET",exports.AnnouncementURL,{
+        headers:header
+    })).data;
+    tmp.StringVersion = JSON.stringify(tmp);
+    return tmp;
+}
+exports.AppVersion = async function() {
+    let tmp = (await axios("GET",exports.AppVersionURL)).data;
+    tmp.StringVersion = JSON.stringify(tmp);
+    return tmp;
+}
+var configKeys = [
+    "token",
+    "client_type",
+    "device_name",
+    "device_model",
+    "sys_version",
+    "channel"
+] 
+
+exports.getGlobalConfig = function() {
+    try {
+        var globalConfig = fs.readFileSync("global.json")
+    } catch(e) {
+        if(e.toString().includes("Error: ENOENT: no such file or directory")) {
+            log.error(`读取配置失败!找不到全局配置文件`)
+        } else {
+            log.error(`读取配置失败!错误信息：${e}`);
+        }
+        exit()
+    }
+    return JSON.parse(globalConfig);
+}
+
+exports.getConfigs = function(){
+    // var configsList;
+    try {
+        var configsList = fs.readdirSync("configs")
+    } catch(e) {
+        if(e == "Error: ENOENT: no such file or directory, scandir 'configs'") {
+            log.error(`读取配置失败!找不到configs文件夹`)
+        } else {
+            log.error(`读取配置失败!错误信息：${e}`);
+        }
+        exit()
+    }
+    log.info(`检测到${configsList.length}个配置文件：`)
+    configsList.forEach(file => {
+        log.info(`${file}`)
+    })
+    var configs = {}
+    configsList.forEach(file => {
+        configs[file] = JSON.parse(fs.readFileSync(`configs/${file}`))
+    })
+    return configs
+}
+exports.checkConfigs = function(configs){
+    for(file in configs) {
+        var configThis = configs[file];
+        var isNoProbem = true;
+        for(key in configKeys) {
+            if(configThis[configKeys[key]] == "" || configThis[configKeys[key]] == undefined || configThis[configKeys[key]] == null || configThis[configKeys[key]] == NaN) {
+                log.error(`配置文件 ${file} 异常：`);
+                log.error(`  —— ${configKeys[key]}字段缺失`);
+                // isNoProbem = false;
+            }
+        }
+        
+        // if(!isNoProbem) {
+        //     exit();
+        // }
+    }
+}
+
+// var appversion = exports.AppVersion();
+
+exports.makeHeader = function(data,appversion){
+    return {
+        'x-rpc-combo_token': data.token,
+        'x-rpc-client_type': data.client_type,
+        'x-rpc-app_version': appversion,
+        'x-rpc-sys_version': data.sys_version,
+        'x-rpc-channel': data.channel,
+        'x-rpc-device_id': data.device_id,
+        'x-rpc-device_name': data.device_name,
+        'x-rpc-device_model': data.device_model,
+        'x-rpc-app_id': 1953439974,
+        'Referer': 'https://app.mihoyo.com',
+        'Host': 'api-cloudgame.mihoyo.com',
+        'Connection': 'Keep-Alive',
+        'Accept-Encoding': 'gzip',
+        'User-Agent': 'okhttp/4.9.0'
+    }
+}
+
+exports.SendLog = function(transporter,mailfrom,mailto,successNum,totalNum,content) {
+    transporter.sendMail({
+        from: mailfrom+'" Genshin CloudPlay Helper"', //邮件来源
+        to: mailto, //邮件发送到哪里，多个邮箱使用逗号隔开
+        subject: `今日已签到${successNum}/${totalNum}名用户`, // 邮件主题
+        text: '☺️😍😎', // 存文本类型的邮件正文
+        html: `${content}` // html类型的邮件正文
+    }, (error, info) => {
+        if (error) {
+        return console.log(error);
+        }
+        log.info("日志已丢出！")
+    });
+}
